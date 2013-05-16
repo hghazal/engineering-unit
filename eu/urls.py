0811e7b7ae3bf96d98c88bd4526d71d20c9acc77
Hecham Ghazal <hghazal@waveapps.com>
2013-05-16 00:21:27 -0400

    cleanup

--- eu/urls.py
+++ eu/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+# from django.contrib import admin
 
 from home import views
 # from pages import views
 
 
-admin.autodiscover()
+# admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     # url(r'^$', include('public_site.urls')),
-    url(r'^$', views.HomepageView.as_view(), name='home'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.HomeView.as_view(), name='home'),
+    # url(r'^admin/', include(admin.site.urls)),
 )
--- home/views.py
+++ home/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
+
+    def get(self, request):
+        context = {}
+        return self.render_to_response(context)
+
