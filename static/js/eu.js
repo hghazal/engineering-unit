b2d0f3c8631322ddbe286d85d014fe55d934311d
Hecham Ghazal <hghazal@waveapps.com>
2013-05-21 11:00:12 -0400

    added the clients to the eu.js file

--- static/js/eu.js
+++ static/js/eu.js
@@ -23,6 +23,21 @@ jQuery(document).ready(function($) {
 
 });
 
+$(function() {
+    $('#clients-flexslider').flexslider({
+      animation: "slide",
+      easing: "swing",
+      animationLoop: true,
+      itemWidth: 1,
+      itemMargin: 1,
+      minItems: 2,
+      maxItems: 9,
+      controlNav: false,
+      directionNav: false,
+      move: 2
+    });
+});
+
 // Custom sorting plugin
 (function($) {
   $.fn.sorted = function(customOptions) {
